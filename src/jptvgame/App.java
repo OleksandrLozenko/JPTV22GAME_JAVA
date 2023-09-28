27753aec7630c4bb34e67aaa0eb5bb5e5800b04d
Oleksandr Lozenko <131644673+OleksandrLozenko@users.noreply.github.com>
2023-09-28 11:08:14 +0300

    Update App.java

--- src/jptvgame/App.java
+++ src/jptvgame/App.java
@@ -10,8 +10,12 @@ public class App {
     public void run() {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        
         System.out.println("-----    GAME    -----");
-        System.out.println("Программа загадала цифру! от 1 до 9;");
+        System.out.println("Программа загадала число! от 1 до 9;");
+        System.out.println("Попробуй угадать!");
+        System.out.println();
+
         int number = random.nextInt(9-0+1) - 0;
         int attempts = 10;
         int count = 0;
@@ -19,36 +23,46 @@ public class App {
         
         while (repeat) {
             if (scanner.hasNextInt()) {
+  
                 int answer = scanner.nextInt();
                 attempts--;
                 count++;
-                if (answer == number) {
+
+                if (answer > number) {
+                    System.out.println("Заданое число меньше!");}
+
+                else if (answer < number) {
+                    System.out.println("Заданое число больше!");}
+   
+                else if (answer == number) {
                     System.out.println("-------------------------------");
                     System.out.println("Угадал! Число -> " + number);
                     System.out.println("Всего использованно попыток -> " + count);
                     System.out.println("-------------------------------");
-                    repeat = false;
-                }
+                    repeat = false;}
+
                 else {
                     System.out.println("-------------------------------");
                     System.out.println("Неугадал! Попробуй еще! Попыток - " + attempts);
-                    System.out.println("-------------------------------");
-                }
+                    System.out.println("-------------------------------");}
+
                 if (attempts == 0) {
                     System.out.println("Вы проиграли =(");
                     repeat = false;
-                    System.out.println("-------------------------------");
-                }
+                    System.out.println("-------------------------------");}    
             }
+            
+            else {
+                System.out.println("Введите цифру!");}
+
+            System.out.println("-------------------------------");
             System.out.println("Чтобы выйти из программы нажмите - q!");
             System.out.println("-------------------------------");
+
             String q = scanner.nextLine();
             if (q.equals("q")) {
-                repeat = false;
-            }
-            else {
-                System.out.println("Введите цифру!");
-            }
+                repeat = false;}
+            
         }
         System.out.println("-----    EXIT    -----");
     }   

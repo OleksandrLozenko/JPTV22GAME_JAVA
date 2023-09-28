a1876f5f19653380bed9225d12ca792cb01c18a7
pupil <pupil@ivkhk.ee>
2023-09-28 10:15:37 +0300

    first commit

---
+++ src/jptvgame/App.java
@@ -0,0 +1,55 @@
+package jptvgame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+    }
+
+    public void run() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("-----    GAME    -----");
+        System.out.println("Программа загадала цифру! от 1 до 9;");
+        int number = random.nextInt(9-0+1) - 0;
+        int attempts = 10;
+        int count = 0;
+        boolean repeat = true;
+        
+        while (repeat) {
+            if (scanner.hasNextInt()) {
+                int answer = scanner.nextInt();
+                attempts--;
+                count++;
+                if (answer == number) {
+                    System.out.println("-------------------------------");
+                    System.out.println("Угадал! Число -> " + number);
+                    System.out.println("Всего использованно попыток -> " + count);
+                    System.out.println("-------------------------------");
+                    repeat = false;
+                }
+                else {
+                    System.out.println("-------------------------------");
+                    System.out.println("Неугадал! Попробуй еще! Попыток - " + attempts);
+                    System.out.println("-------------------------------");
+                }
+                if (attempts == 0) {
+                    System.out.println("Вы проиграли =(");
+                    repeat = false;
+                    System.out.println("-------------------------------");
+                }
+            }
+            System.out.println("Чтобы выйти из программы нажмите - q!");
+            System.out.println("-------------------------------");
+            String q = scanner.nextLine();
+            if (q.equals("q")) {
+                repeat = false;
+            }
+            else {
+                System.out.println("Введите цифру!");
+            }
+        }
+        System.out.println("-----    EXIT    -----");
+    }   
+}
---
+++ src/jptvgame/JPTVGame.java
@@ -0,0 +1,9 @@
+package jptvgame;
+class JPTVGame {
+    public static void main(String[] args) {
+        App app;
+        app = new App();
+        app.run();  
+        }
+    }
+
